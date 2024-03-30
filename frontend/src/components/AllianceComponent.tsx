ae7f5e35b050589b83ffd4318caf4fd439fe6f11
Robert Sim <robert.sim@gmail.com>
2024-03-29 22:31:23 -0700

    fix root url

--- frontend/src/components/AllianceComponent.tsx
+++ frontend/src/components/AllianceComponent.tsx
@@ -77,7 +77,7 @@ function AllianceComponent() {
     console.log(payload);
     // POST alliances to /model/district_model_event_match_type/bracket
     // TODO: this environment variable can't exist in the browser- how to set it properly at build time?
-    let baseUrl = process.env.REACT_APP_API_BASE_URL || '/';
+    let baseUrl = process.env.REACT_APP_API_BASE_URL || '';
     let url = `${baseUrl}/model/${district}_${modelEvent}_${matchType}/bracket/${bracketMethod}`;
     // POST the alliances to the url using fetch:
     fetch(url, {
@@ -102,7 +102,7 @@ function AllianceComponent() {
     var blue = slots[a2].map(slot => slot.team ? slot.team.team : '').join(',');
 
     // POST alliances to /model/district_model_event_match_type/bracket
-    let baseUrl = process.env.REACT_APP_API_BASE_URL || '/';
+    let baseUrl = process.env.REACT_APP_API_BASE_URL || '';
     // /model/<model_key>/predict/<red>/<blue>
     let url = `${baseUrl}/model/${district}_${modelEvent}_${matchType}/predict/${red}/${blue}/${modelMethod}`;
     // POST the alliances to the url using fetch:
