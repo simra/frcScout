f4fc511e4ba2878565cee4c7a9fa1ee6907af679
Robert Sim <robert.sim@gmail.com>
2024-03-29 19:39:43 -0700

    add todo note

--- frontend/src/components/AllianceComponent.tsx
+++ frontend/src/components/AllianceComponent.tsx
@@ -76,6 +76,7 @@ function AllianceComponent() {
     }, {});
     console.log(payload);
     // POST alliances to /model/district_model_event_match_type/bracket
+    // TODO: this environment variable can't exist in the browser- how to set it properly at build time?
     let baseUrl = process.env.REACT_APP_API_BASE_URL || '/';
     let url = `${baseUrl}/model/${district}_${modelEvent}_${matchType}/bracket/${bracketMethod}`;
     // POST the alliances to the url using fetch:
